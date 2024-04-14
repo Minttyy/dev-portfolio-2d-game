4c1ab8623b48f8121840e7906c48b59866b3f83d
Sabrina T <smin1999@gmail.com>
2024-04-14 22:04:03 +1200

    finished project - need to edit dialogue

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,14 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  book: `I like to read self development books like .....`,
+  fridge: `My favourite snacks are matcha related and korean snacks!`,
+  tv: `I enjoy watching korean dramas`,
+  bachelorDegree: `I have a Bachelor degree in Mechatronics Engineering from University of Auckland, New Zealand`,
+  mastersDegree: `I recently completed my Masters degree in Applied Computing with Lincoln University, Christchurch`,
+  computer: `I enjoy learning new coding language and self improvement content`,
+  bed: `This is my resting space, take a break!`,
+  resume: `This is my resume and linkedin page: .........`,
+  nature2: `I enjoy going for walks for fresh walks over the day`,
+  nature3: `Seeing the green nature and blue skies, calms me down`,
+};
